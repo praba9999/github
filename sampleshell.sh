81e61be74c71050c6c1a27a89bce18ce5250b370
prabhakar <prabhakar.gungi9@gmail.com>
2022-11-12 19:16:00 +0530

    sample shell

---
+++ sampleshell.sh
@@ -0,0 +1,3 @@
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
+CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
